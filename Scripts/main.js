d96bfd87f86d28e3c9de77d8eb38a72f2af47673
Christopher Sardegna <gpg@reagentx.net>
2020-09-24 23:42:20 +0000

    Add missing semicolons

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,29 +1,29 @@
 nova.commands.register("autopep8.runAutopep8", (workspace) => {
-    const enabled=nova.config.get('com.chrissardegna.autopep8Enabled')
-    const autopep8=nova.config.get('com.chrissardegna.autopep8ExecPath')
-    const autopep8Args=nova.config.get('com.chrissardegna.autopep8Args')
-    let currentFile=workspace.activeTextEditor.document.path
+    const enabled = nova.config.get('com.chrissardegna.autopep8Enabled');
+    const autopep8 = nova.config.get('com.chrissardegna.autopep8ExecPath');
+    const autopep8Args = nova.config.get('com.chrissardegna.autopep8Args');
+    let currentFile = workspace.activeTextEditor.document.path;
 
     if (currentFile) {
-        let cliArgs = ['--in-place']
+        let cliArgs = ['--in-place'];
 
         if (autopep8Args) {
-            const parsedAutopep8Args = autopep8Args.split(' ')
-            parsedAutopep8Args.push(currentFile)
-            cliArgs.push(...parsedAutopep8Args)
+            const parsedAutopep8Args = autopep8Args.split(' ');
+            parsedAutopep8Args.push(currentFile);
+            cliArgs.push(...parsedAutopep8Args);
         } else {
-            cliArgs.push(currentFile)
+            cliArgs.push(currentFile);
         }
 
-        console.log(`Executing ${autopep8} ${cliArgs.join(' ')}`)
+        console.log(`Executing ${autopep8} ${cliArgs.join(' ')}`);
 
         try {
             let p = new Process(autopep8, {
                 args: cliArgs
             })
-            p.start()
-        } catch(err) {
-            console.error(`autopep8 error: ${err}`)
+            p.start();
+        } catch (err) {
+            console.error(`autopep8 error: ${err}`);
         }
     }
 })
