d07b74693ec0cf8757910cdfdc87e1ca8de59c22
Christopher Sardegna <gpg@reagentx.net>
2020-09-25 00:02:35 +0000

    Send notifications when work completes

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,3 +1,11 @@
+const notify = body => {
+    let request = new NotificationRequest("autopep8-formatter");
+    request.title = nova.localize("autopep8");
+    request.body = nova.localize(body);
+    let promise = nova.notifications.add(request);
+}
+
+
 nova.commands.register("autopep8.runAutopep8", (workspace) => {
     const enabled = nova.config.get('com.chrissardegna.autopep8Enabled');
     const autopep8 = nova.config.get('com.chrissardegna.autopep8ExecPath');
@@ -21,9 +29,16 @@ nova.commands.register("autopep8.runAutopep8", (workspace) => {
             let p = new Process(autopep8, {
                 args: cliArgs
             })
+
+            p.onDidExit(function() {
+                notify("Formatting current file.");
+            });
+
             p.start();
         } catch (err) {
             console.error(`autopep8 error: ${err}`);
+            notify("Unable to format current file.");
         }
+        
     }
 })
